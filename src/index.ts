b9edb873a49e018bd8d5055e17a6c53a7805cb3f
Alex Duan <alexduan@Duanamo-AI.local>
2025-12-09 19:47:05 -0500

    begin cleanup of crappily generated plugins

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@
  * Exports the plugin instance as default export per Holo plugin contract.
  */
 
-export { OllamaProviderPlugin } from './plugin.js';
+export {OllamaProviderPlugin} from './plugin.js';
